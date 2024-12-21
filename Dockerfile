d8b4397bf7d6ae8410f3bf8dcec9c28bd807d6b9
eduardo-pitanga <edudupilo@gmail.com>
2024-12-21 18:11:41 -0300

    docker ready

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+﻿# Etapa 1: Build
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build-env
+WORKDIR /app
+
+# Expõe a porta usada pela aplicação
+EXPOSE 8080
+EXPOSE 8081
+
+# Copiar arquivos do projeto
+COPY *.sln ./
+COPY eventos_ger/eventos_ger.csproj ./
+RUN dotnet restore eventos_ger.csproj
+
+# Copiar todo o código
+COPY . ./
+RUN dotnet publish eventos_ger/eventos_ger.csproj -c Release -o out
+
+# Etapa 2: Runtime
+FROM mcr.microsoft.com/dotnet/aspnet:8.0
+WORKDIR /app
+COPY --from=build-env /app/out .
+
+# Comando para rodar a aplicação
+ENTRYPOINT ["dotnet", "eventos_ger.dll"]
--- eventos_ger/Aplication/Program.cs
+++ eventos_ger/Aplication/Program.cs
@@ -3,29 +3,35 @@ using eventos_ger.Repository;
 using eventos_ger.Repository.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<Ger_Evento_Bd>(opt => opt.UseInMemoryDatabase("Gerenciamento"));
-
-// Registra o repositório no contêiner de dependências
-builder.Services.AddScoped<IParticipanteRepository, ParticipanteRepository>();
-builder.Services.AddScoped<IEventoRepository, EventoRepository>(); 
-builder.Services.AddScoped<IPalestranteRepository, PalestranteRepository>();
-builder.Services.AddScoped<IOrganizadorRepository, OrganizadorRepository>();
-builder.Services.AddScoped<ILocalRepository, LocalRepository>();
-builder.Services.AddScoped<IAssociacaoEventoPessoa, AssociacaoEventoPessoaRepository>();
-
-// Registra outros serviços
-builder.Services.AddControllers();
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+public partial class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<Ger_Evento_Bd>(opt => opt.UseInMemoryDatabase("Gerenciamento"));
+
+        // Registra o repositório no contêiner de dependências
+        builder.Services.AddScoped<IParticipanteRepository, ParticipanteRepository>();
+        builder.Services.AddScoped<IEventoRepository, EventoRepository>(); 
+        builder.Services.AddScoped<IPalestranteRepository, PalestranteRepository>();
+        builder.Services.AddScoped<IOrganizadorRepository, OrganizadorRepository>();
+        builder.Services.AddScoped<ILocalRepository, LocalRepository>();
+        builder.Services.AddScoped<IAssociacaoEventoPessoa, AssociacaoEventoPessoaRepository>();
+
+        // Registra outros serviços
+        builder.Services.AddControllers();
+        var app = builder.Build();
+
+        // if (app.Environment.IsDevelopment())
+        // {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        // }
+        app.UseHttpsRedirection();
+        app.MapControllers();
+        app.Run();
+    }
 }
-app.UseHttpsRedirection();
-app.MapControllers();
-app.Run();
