378baac67b27755dff8b1100869faebc55e9f6fe
eduardo-pitanga <edudupilo@gmail.com>
2025-02-09 18:31:31 -0300

    id local

--- eventos_ger/Model/DTOs/Response/eventoResponse.cs
+++ eventos_ger/Model/DTOs/Response/eventoResponse.cs
@@ -7,6 +7,7 @@
         public string? Descricao { get; set; }
         public string? Data { get; set; }
         public string? Horario { get; set; }
+        public int? idLocal { get; set; }
         public string? NomeLocal { get; set; }
         public int IdOrganizador { get; set; }
 
